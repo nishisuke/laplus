3093c383b7fc8100c8192ca372108ba3a96c2122
nishisuke <ryoskns+git@gmail.com>
2019-07-12 15:53:38 +0900

    mv pry command to extension

--- lib/laplus.rb
+++ lib/laplus.rb
@@ -19,13 +19,4 @@ require 'Laplus/extensions/method_extension'
   klass.include(Laplus::MethodExtension)
 end
 
-begin
-  require 'pry'
-
-  Pry::Commands.block_command :lp_def, 'show arg method definition' do |method_str|
-    m = target.eval(method_str)
-    definition = m.definition
-    output.p definition
-  end
-rescue LoadError
-end
+require 'laplus/extensions/pry_command_extension'
