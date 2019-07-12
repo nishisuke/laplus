fd2a386a75f36d0f7e609e5752af20e0ff135479
nishisuke <ryoskns+git@gmail.com>
2019-07-12 11:03:47 +0900

    add pry command

--- lib/laplus.rb
+++ lib/laplus.rb
@@ -18,3 +18,14 @@ require 'Laplus/extensions/method_extension'
 [Method, UnboundMethod].each do |klass|
   klass.include(Laplus::MethodExtension)
 end
+
+begin
+  require 'pry'
+
+  Pry::Commands.block_command :lp_def, 'show arg method definition' do |method_str|
+    m = target.eval(method_str)
+    definition = m.definition
+    output.p definition
+  end
+rescue LoadError
+end
