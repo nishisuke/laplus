fa664bb88c6d1b6c7db084c28476fe4b8d77a430
nishisuke <ryoskns+git@gmail.com>
2019-07-16 09:07:54 +0900

    fix info

--- lib/laplus/inspect_strategy/method_strategy.rb
+++ lib/laplus/inspect_strategy/method_strategy.rb
@@ -13,21 +13,31 @@ module Laplus
       def inspect
         method = inspector.object
         source_location = method.source_location
+        path, line = source_location
 
         snippet = if source_location.nil?
                     '(defined in clang)'
-                  elsif !File.exist?(source_location.first)
+                  elsif !File.exist?(path)
                     "defined at #{path}. but no such file."
                   else
-                    path, line = source_location
                     Source.new(path).snip_code_at(line)
                   end
 
         lines = snippet.split("\n")
         snippet_offset_indent = UI::IndentionLines.new(lines).offset.indent(1).join("\n")
 
+        super_methods = [method]
+        while super_method = super_methods.last.super_method
+          super_methods << super_method
+        end
+        super_methods.reverse!
+
         <<~DEFINITION
-          #{method.inspect}
+
+          #{path}:#{line}
+
+          super_methods:
+          #{super_methods.map { |m|  '  ' + m.inspect.match(/Method: (.+)>\z/).to_a[1] }.join("\n")}
 
           #{method.owner.class.name.downcase} #{method.owner.name}
           #{snippet_offset_indent.chomp}
