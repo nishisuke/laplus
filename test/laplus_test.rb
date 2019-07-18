67d1317d5b82fb308e59a6ddc23acf48e38dd5be
nishisuke <ryoskns+git@gmail.com>
2019-07-18 15:02:38 +0900

    fix test

--- test/laplus_test.rb
+++ test/laplus_test.rb
@@ -6,16 +6,33 @@ class LaplusTest < Minitest::Test
   end
 end
 
-describe Laplus do
-  describe 'Method#lp' do
-    it 'returns Laplus::Inspector object' do
-      assert_instance_of Laplus::Inspector, 0.method(:to_s).lp
-    end
+describe 'Method' do
+  it 'includes Laplus::Extension' do
+    assert_includes Method, Laplus::Extension
   end
+end
 
-  describe 'UnboundMethod#lp' do
+describe 'UnboundMethod' do
+  it 'includes Laplus::Extension' do
+    assert_includes UnboundMethod, Laplus::Extension
+  end
+end
+
+describe 'Proc' do
+  it 'includes Laplus::Extension' do
+    assert_includes Proc, Laplus::Extension
+  end
+end
+
+describe Laplus::Extension do
+  describe '#lp' do
+    before do
+      @object = Object.new
+      @object.extend Laplus::Extension
+    end
+
     it 'returns Laplus::Inspector object' do
-      assert_instance_of Laplus::Inspector, Class.instance_method(:include).lp
+      assert_instance_of Laplus::Inspector, @object.lp
     end
   end
 end
